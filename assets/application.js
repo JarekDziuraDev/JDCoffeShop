709d4ac1aff693b47dfe4245c8bfc09113e1ff84
JarekDziuraDev <jarek.dziura.dev@gmail.com>
2021-12-21 00:25:43 +0100

    implements a dynamic product counter in the header

--- assets/application.js
+++ assets/application.js
@@ -1,27 +1,51 @@
-var addToCartForm = document.querySelector("#addToCartForm");
 
-addToCartForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+const itemSlide = document.getElementsByClassName("carousel-cell");
 
-    let formData = {
-        'items': [
-            {
-                'id': document.getElementById("itemId").value,
-                'quantity': 1
-            }
-        ]
-    };
-    console.log(formData);
-    
-    fetch('/cart/add.js', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+for (let item of itemSlide ) {
+    const addToCartForm = item.querySelector('#addToCartForm');
+
+    const id = addToCartForm.children[0].value;
+
+    addToCartForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        let formData = {
+                'items': [
+                    {
+                        'id': id,
+                        'quantity': 1
+                    }
+                ]
+            };
+            
+            fetch('/cart/add.js', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            .then( (resp) => { 
+                return resp.json(); 
+            })
+            .then( (resp) => {
+                update_cart();
+            })
+            .catch( (err) => {
+                console.log('ERROR: ' + err );
+            })
     })
-    .then( (resp) => resp.json())
-    .catch( (err) => {
-        console.log('ERROR: ' + err );
-    })
+}
+
+// const addToCartForm = document.getElementById('addToCartForm')
+
+document.addEventListener('DOMContentLoaded', () => {
+    update_cart();
 })
+
+function update_cart() {
+    fetch('/cart.js')
+    .then( (resp) => resp.json())
+    .then( (data) => document.getElementById("numberOfCartItems").innerHTML = data.items.length)
+    .catch( (err) => console.log(err))
+}
